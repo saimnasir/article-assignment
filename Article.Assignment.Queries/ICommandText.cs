ded548574d4fc5c2432461e39e9ad1815d43b30d
saimnasir <saim.nasir.65@gmail.com>
2020-04-19 23:47:04 +0300

    CommandText for queries added

---
+++ Article.Assignment.Queries/ICommandText.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Article.Assignment.Queries
+{
+
+    public interface ICommandText
+    {
+        #region Article 
+        string CreateArticleCommand { get; }
+        string ReadArticleCommand { get; }
+        string UpdateArticleCommand { get; }
+        string DeleteArticleCommand { get; }
+        string ListAllArticlesCommand { get; }
+        string SearchArticlesCommand { get; }
+        #endregion
+
+        #region Author
+        string CreateAuthorCommand { get; }
+        string ReadAuthorCommand { get; }
+        string UpdateAuthorCommand { get; }
+        string DeleteAuthorCommand { get; }
+        string ListAllAuthorsCommand { get; }
+
+        #endregion
+
+    }
+}
