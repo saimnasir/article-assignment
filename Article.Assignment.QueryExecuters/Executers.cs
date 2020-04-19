98c496637babffb373e49379785237a306235b5c
saimnasir <saim.nasir.65@gmail.com>
2020-04-19 23:47:46 +0300

    Query executer added

---
+++ Article.Assignment.QueryExecuters/Executers.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Article.Assignment.QueryExecuters
+{
+    public class Executers : IExecuters
+    {
+        public void ExecuteCommand(string connStr, Action<SqlConnection> task)
+        {
+            using (var conn = new SqlConnection(connStr))
+            {
+                conn.Open();
+                task(conn);
+            }
+        }
+
+        public T ExecuteCommand<T>(string connStr, Func<SqlConnection, T> task)
+        {
+            using (var conn = new SqlConnection(connStr))
+            {
+                conn.Open();
+                return task(conn);
+            }
+        }
+
+    }
+}
---
+++ Article.Assignment.QueryExecuters/IExecuters.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+
+namespace Article.Assignment.QueryExecuters
+{
+
+    public interface IExecuters
+    {
+        void ExecuteCommand(string connStr, Action<SqlConnection> task);
+        T ExecuteCommand<T>(string connStr, Func<SqlConnection, T> task);
+
+    }
+}
